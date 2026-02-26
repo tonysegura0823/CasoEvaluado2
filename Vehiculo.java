90e2b1075dcc370a5ae7e334a1ee9ddf6ffaf734
tonysegura0823 <tonysegura0823@gmail.com>
2026-02-25 20:36:57 -0600

    Add files via upload

--- Vehiculo.java
+++ Vehiculo.java
@@ -10,4 +10,51 @@ package estudiocaso1;
  */
 public class Vehiculo {
     
+    package alquilervehiculos;
+    public class Vehiculo {
+
+    private String placa;
+    private String marca;
+    private double precioPorDia;
+    private int dias;
+    private boolean seguroCompleto;
+    private boolean gps;
+
+    public Vehiculo(String placa, String marca, double precioPorDia, int dias, boolean seguroCompleto, boolean gps) {
+        this.placa = placa;
+        this.marca = marca;
+        this.precioPorDia = precioPorDia;
+        this.dias = dias;
+        this.seguroCompleto = seguroCompleto;
+        this.gps = gps;
+    }
+
+    public double calcularSubtotal() {
+        double costoBase = precioPorDia * dias;
+        if (dias > 7) {
+            costoBase = costoBase - (costoBase * 0.15);
+        }
+        double recargos = 0;
+        if (seguroCompleto) {
+            recargos += 5000 * dias;
+        }
+        if (gps) {
+            recargos += 2000 * dias;
+        }
+        return costoBase + recargos;
+    }
+
+    public int calcularPuntos() {
+        int puntos = 0;
+        if (dias > 7) {
+            puntos += 5;
+        }
+        if (seguroCompleto) {
+            puntos += 2;
+        }
+        return puntos;
+    }
+
+    public String getPlaca() {
+        return placa;
 }
